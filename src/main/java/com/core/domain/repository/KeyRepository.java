9926706c742479d068ec2e7508b106d0ccf9c9db
pauloJ34 <paulofranklin612@gmail.com>
2023-06-07 16:07:47 -0300

    refactor: authentication communication

--- src/main/java/com/core/domain/repository/KeyRepository.java
+++ src/main/java/com/core/domain/repository/KeyRepository.java
@@ -5,7 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.core.domain.entities.Key;
 
-
-
 public interface KeyRepository extends JpaRepository<Key, UUID>{
 }
